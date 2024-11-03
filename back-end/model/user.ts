84366cba5cea38522a99a528d5cf8fd8397980d3
Dominik-Pokol <r0992657@ucll.be>
2024-11-03 17:10:45 +0100

    user model validation added

--- back-end/model/user.ts
+++ back-end/model/user.ts
@@ -1,14 +1,43 @@
 export class User {
+    private static users: User[] = [];
     private id?: number;
     private username: string;
     private email: string;
     private password: string;
 
     constructor(user: { id?: number; username: string; email: string; password: string }) {
+        this.validatie(user);
+
         this.id = user.id;
         this.username = user.username;
         this.email = user.email;
         this.password = user.password;
+
+        User.users.push(this);
+    }
+    validatie(user: { id?: number; username: string; email: string; password: string }) {
+        // field validaton:
+        if (!user.username?.trim()) {
+            throw new Error('Username is required.');
+        }
+        if (!user.email?.trim()) {
+            throw new Error('Email is required.');
+        }
+        if (!user.password?.trim()) {
+            throw new Error('Password is required.');
+        }
+        //pw lenght
+        if (user.password.length < 8) {
+            throw new Error('Password must be at least 8 characters long');
+        }
+        // uniqueness validation
+        // Uniqueness validation
+        if (User.users.some((existingUser) => existingUser.username === user.username)) {
+            throw new Error('Username must be unique.');
+        }
+        if (User.users.some((existingUser) => existingUser.email === user.email)) {
+            throw new Error('Email must be unique.');
+        }
     }
 
     getId(): number | undefined {
