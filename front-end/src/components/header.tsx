5caf1508e43c455b45ed6b9637f66184b6ceddbe
Dominik-Pokol <r0992657@ucll.be>
2024-12-20 14:51:08 +0100

    header made it clearer

--- front-end/src/components/header.tsx
+++ front-end/src/components/header.tsx
@@ -1,34 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const Header = () => {
-  return (
-    <header className="bg-white text-black py-6 px-12 w-full flex justify-between items-center shadow-xl">
-      <div className="logo flex items-center space-x-6">
-        <img src="/posters/logo_cinescope.jpg" alt="Cinescope Logo" className="h-12" />
-        <h1 className="text-4xl font-extrabold tracking-wide text-black">Cinescope</h1>
-      </div>
-      <nav>
-        <ul className="flex space-x-12">
-          <li>
-            <Link href="/" className="text-lg font-semibold hover:bg-gray-200 hover:text-black py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105">Home</Link>
-          </li>
-          <li>
-            <Link href="/movie" className="text-lg font-semibold hover:bg-gray-200 hover:text-black py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105">Movies</Link>
-          </li>
-          <li>
-            <Link href="/favorites" className="text-lg font-semibold hover:bg-gray-200 hover:text-black py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105">Favorites</Link>
-          </li>
-          <li>
-            <Link href="/login" className="text-lg font-semibold hover:bg-gray-200 hover:text-black py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105">Login</Link>
-          </li>
-          <li>
-            <Link href="/register" className="text-lg font-semibold hover:bg-gray-200 hover:text-black py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105">Register</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+const Header: React.FC = () => {
+    const router = useRouter();
+    const [loggedInUser, setLoggedInUser] = useState<string | null>(null);
+
+    useEffect(() => {
+        const user = sessionStorage.getItem('loggedInUser');
+        setLoggedInUser(user);
+    }, [router.asPath]);
+    // By including router.asPath, this effect runs every time the route changes.
+
+    const handleLogout = () => {
+        sessionStorage.removeItem('loggedInUser');
+        sessionStorage.removeItem('authToken');
+        setLoggedInUser(null);
+        router.push('/'); // Redirect to home (or another page) after logout if desired.
+    };
+
+    return (
+        <header className="bg-white text-black py-6 px-12 w-full flex justify-between items-center shadow-xl">
+            <div className="logo flex items-center space-x-6">
+                <img src="/posters/logo_cinescope.jpg" alt="Cinescope Logo" className="h-12" />
+                <h1 className="text-4xl font-extrabold tracking-wide text-black">Cinescope</h1>
+            </div>
+            <nav>
+                <ul className="flex space-x-12 items-center">
+                    <li>
+                        <Link
+                            href="/"
+                            className="text-lg font-semibold hover:bg-gray-200 hover:text-black py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105"
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/movie"
+                            className="text-lg font-semibold hover:bg-gray-200 hover:text-black py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105"
+                        >
+                            Movies
+                        </Link>
+                    </li>
+
+                    {loggedInUser ? (
+                        <>
+                            <li>
+                                <Link
+                                    href="/favorites"
+                                    className="text-lg font-semibold hover:bg-gray-200 hover:text-black py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105"
+                                >
+                                    Favorites
+                                </Link>
+                            </li>
+                            <li className="text-lg font-semibold text-gray-700">
+                                Welcome, {loggedInUser}!
+                            </li>
+                            <li>
+                                <Link
+                                    href="/"
+                                    onClick={handleLogout}
+                                    className="cursor-pointer text-lg font-semibold hover:bg-gray-200 hover:text-black py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105"
+                                >
+                                    Logout
+                                </Link>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li>
+                                <Link
+                                    href="/login"
+                                    className="text-lg font-semibold hover:bg-gray-200 hover:text-black py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105"
+                                >
+                                    Login
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href="/register"
+                                    className="text-lg font-semibold hover:bg-gray-200 hover:text-black py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105"
+                                >
+                                    Register
+                                </Link>
+                            </li>
+                        </>
+                    )}
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
--- front-end/src/pages/login/index.tsx
+++ front-end/src/pages/login/index.tsx
@@ -1,103 +1,97 @@
-import React, { useState } from "react";
-import { useRouter } from "next/router"; // Gebruik useRouter van Next.js
-import { login } from "@services/loginService"; // Importeer de login service
+import React, { useState } from 'react';
+import { useRouter } from 'next/router'; // Gebruik useRouter van Next.js
+import { login } from '@services/loginService'; // Importeer de login service
 
 const Login: React.FC = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const router = useRouter(); // Gebruik useRouter in plaats van useNavigate
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const router = useRouter(); // Gebruik useRouter in plaats van useNavigate
 
-  const validateForm = () => {
-    setError("");
+    const validateForm = () => {
+        setError('');
 
-    if (!email.includes("@")) {
-      setError("Please enter a valid email address.");
-      return false;
-    }
+        if (!email.includes('@')) {
+            setError('Please enter a valid email address.');
+            return false;
+        }
 
-    if (password.length < 5) {
-      setError("Password must be at least 8 characters long.");
-      return false;
-    }
+        if (password.length < 5) {
+            setError('Password must be at least 8 characters long.');
+            return false;
+        }
 
-    return true;
-  };
+        return true;
+    };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setError(""); // Reset errors
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setError(''); // Reset errors
 
-    if (!validateForm()) {
-      return;
-    }
+        if (!validateForm()) {
+            return;
+        }
 
-    try {
-      const { username, token } = await login(email, password); // Service aanroepen
-      console.log("Logged in as:", username, "Token:", token);
+        try {
+            const { username, token } = await login(email, password); // Service aanroepen
+            console.log('Logged in as:', username, 'Token:', token);
 
-      // Token opslaan in localStorage of cookies
-      localStorage.setItem("authToken", token);
+            // Token opslaan in localStorage of cookies
+            localStorage.setItem('authToken', token);
+            sessionStorage.setItem('loggedInUser', username);
+            // Redirect naar een andere pagina, bijvoorbeeld een dashboard
+            router.push('/movie');
+        } catch (err: any) {
+            setError(err.message || 'An unexpected error occurred.');
+        }
+    };
 
-      // Redirect naar een andere pagina, bijvoorbeeld een dashboard
-      router.push("/movie");
-    } catch (err: any) {
-      setError(err.message || "An unexpected error occurred.");
-    }
-  };
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 via-green-500 to-teal-600 p-6 animate-fade-in">
+            <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-xl transform scale-100 transition-all duration-500 hover:scale-105">
+                <h2 className="text-4xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-teal-700 animate-pulse">
+                    Log In
+                </h2>
 
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 via-green-500 to-teal-600 p-6 animate-fade-in">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-xl transform scale-100 transition-all duration-500 hover:scale-105">
-        <h2 className="text-4xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-teal-700 animate-pulse">
-          Log In
-        </h2>
+                {error && (
+                    <p className="text-red-600 text-center text-sm animate-slide-down">{error}</p>
+                )}
 
-        {error && (
-          <p className="text-red-600 text-center text-sm animate-slide-down">
-            {error}
-          </p>
-        )}
+                <form onSubmit={handleSubmit} className="space-y-6">
+                    <div>
+                        <label className="block text-sm font-medium text-teal-700">Email</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="w-full px-4 py-2 mt-1 text-teal-700 bg-teal-50 border border-teal-200 rounded-lg shadow-md focus:outline-none focus:ring-4 focus:ring-blue-400 focus:ring-opacity-50 hover:shadow-lg transition-all duration-300"
+                            required
+                            placeholder="you@example.com"
+                        />
+                    </div>
 
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div>
-            <label className="block text-sm font-medium text-teal-700">
-              Email
-            </label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-2 mt-1 text-teal-700 bg-teal-50 border border-teal-200 rounded-lg shadow-md focus:outline-none focus:ring-4 focus:ring-blue-400 focus:ring-opacity-50 hover:shadow-lg transition-all duration-300"
-              required
-              placeholder="you@example.com"
-            />
-          </div>
+                    <div>
+                        <label className="block text-sm font-medium text-teal-700">Password</label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="w-full px-4 py-2 mt-1 text-teal-700 bg-teal-50 border border-teal-200 rounded-lg shadow-md focus:outline-none focus:ring-4 focus:ring-blue-400 focus:ring-opacity-50 hover:shadow-lg transition-all duration-300"
+                            required
+                            placeholder="Your password"
+                        />
+                    </div>
 
-          <div>
-            <label className="block text-sm font-medium text-teal-700">
-              Password
-            </label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-2 mt-1 text-teal-700 bg-teal-50 border border-teal-200 rounded-lg shadow-md focus:outline-none focus:ring-4 focus:ring-blue-400 focus:ring-opacity-50 hover:shadow-lg transition-all duration-300"
-              required
-              placeholder="Your password"
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="w-full px-4 py-2 font-bold text-white bg-gradient-to-r from-blue-500 via-green-500 to-teal-500 rounded-lg hover:from-blue-600 hover:to-teal-600 focus:outline-none focus:ring-4 focus:ring-blue-300 shadow-lg transform hover:scale-105 transition-transform duration-300"
-          >
-            Log In
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+                    <button
+                        type="submit"
+                        className="w-full px-4 py-2 font-bold text-white bg-gradient-to-r from-blue-500 via-green-500 to-teal-500 rounded-lg hover:from-blue-600 hover:to-teal-600 focus:outline-none focus:ring-4 focus:ring-blue-300 shadow-lg transform hover:scale-105 transition-transform duration-300"
+                    >
+                        Log In
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
